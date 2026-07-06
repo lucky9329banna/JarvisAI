85bd66ac838dd730d466fa161a4ed4e56c8d4384
Lokendra Singh Sisodiya <singhsisodiyalokendra3@gmail.com>
2026-07-06 12:49:20 +0530

    Debug AI

--- src/App.jsx
+++ src/App.jsx
@@ -85,6 +85,9 @@ function startListening() {
     setQuestion("");
 
     try {
+
+      alert("AI Request Start");
+
       const response = await ai.models.generateContent({
         model: "gemini-2.5-flash",
         contents: userQuestion,
